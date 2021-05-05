0ae898dca0086d9dda68ead721c905ee7ede8656
Dexter Yip <dexteryip2003@yahoo.com.hk>
2021-05-05 23:29:41 +0800

    style change

--- myweb/src/app/app.component.ts
+++ myweb/src/app/app.component.ts
@@ -1,10 +1,38 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
 })
 export class AppComponent {
-  title = 'myweb';
+  isOpen = false;
+  ngOnInit(){
+    
+  }
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
 }
